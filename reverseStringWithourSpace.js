1d7f9abfa9bc33dd3914cdb24e8f36bfdd9575e5
Sameer Ahmed <sameerahmed1601@gmail.com>
2024-10-27 13:32:46 +0530

    reverse String withour spaces

---
+++ reverseStringWithourSpace.js
@@ -0,0 +1,18 @@
+let str = "o n e";
+
+function revereseAStringWithoutSpaces(str){
+    
+    let nstr = "";
+    for(let i=str.length-1;i>=0; i--){
+        if(str[i]!==" "){
+            nstr+= str[i];
+        }
+        // if(str[i]!==" "){
+        //     continue;
+        // }
+        // nstr+=str[i];
+    }
+    return nstr;
+}
+
+console.log(revereseAStringWithoutSpaces(str))
