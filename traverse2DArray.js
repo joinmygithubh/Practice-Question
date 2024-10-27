47bd933c6295964d8f5d7a456bbe61ad06f1be3b
Sameer Ahmed <sameerahmed1601@gmail.com>
2024-10-27 16:11:00 +0530

    traversal of 2D array

--- traverse2DArray.js
+++ traverse2DArray.js
@@ -4,7 +4,6 @@ let narr =[]
 for(let i=0; i<arr.length; i++){
     for(let j=0; j<arr[i].length; j++){
         console.log(arr[i][j])
-        narr.push(arr[i][j])
-        
+        narr.push(arr[i][j]) 
     }
 }console.log(narr)
