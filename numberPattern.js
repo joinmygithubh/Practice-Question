e3d3ab935e9e719b7cb9a7cc7cd2bdf6652ccc93
Sameer Ahmed <sameerahmed1601@gmail.com>
2024-10-28 22:47:38 +0530

    number Pattern

---
+++ numberPattern.js
@@ -0,0 +1,19 @@
+function numberPattern(N){
+    //write your code here
+    for(let i=1; i<=N; i++){
+      let temp=" ";
+      for(let j=1; j<=i; j++){
+        process.stdout.write(j.toString())
+      }
+      console.log();
+    }
+    for(let i=N-1; i>=1; i--){
+      let temp=" ";
+      for(let j=1; j<=i; j++){
+        process.stdout.write(j.toString())
+      }
+      console.log();
+    }
+  }
+
+numberPattern(5)  
