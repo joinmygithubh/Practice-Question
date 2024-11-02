f78b1a01a064775ffcdbb78ec342420f59592fc1
Sameer Ahmed <sameerahmed1601@gmail.com>
2024-11-02 21:08:10 +0530

    Largest Number Identifier

---
+++ largestNumberIdentifier.js
@@ -0,0 +1,26 @@
+function compareNumbers(number1, number2) {
+    if (number1 > number2) {
+        return `${number1} is larger than ${number2}`;
+    } else if (number1 < number2) {
+        return `${number2} is larger than ${number1}`;
+    } else {
+        return "Both numbers are equal";
+    }
+}
+
+// Test cases
+let number1 = 5, number2 = 10;
+console.log(compareNumbers(number1, number2));  //      output: "10 is larger than 5"
+
+number1 = 7, number2 = 7;
+console.log(compareNumbers(number1, number2));  //   output: "Both numbers are equal"
+
+number1 = -1, number2 = -10;
+console.log(compareNumbers(number1, number2));  //     output: "-1 is larger than -10"
+
+//  Edge cases
+number1 = 0, number2 = 0;
+console.log(compareNumbers(number1, number2)); //   output: "Both numbers are equal"
+
+number1 = -5, number2 = -3;
+console.log(compareNumbers(number1, number2)); // output: "-3 is larger than -5"
