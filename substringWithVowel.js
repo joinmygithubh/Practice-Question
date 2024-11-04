440a61caebec6facb1e6a7c8bbaed31b334cd260
Sameer Ahmed <sameerahmed1601@gmail.com>
2024-11-04 20:28:38 +0530

    SubString with Vowel

---
+++ substringWithVowel.js
@@ -0,0 +1,25 @@
+function stringVowels(N,str){
+    //Write code here
+      let count =0;
+      for(let i=0; i<N; i++){
+        let temp=""
+        for(let j=i; j<N; j++){
+          temp+= str[i];
+          if(isStartingWithVowel(temp)){
+            count++;
+          }
+        }
+      }
+      console.log(count);
+    }
+    
+    function isStartingWithVowel(str){
+      if(str[0]=='a' || str[0]=='e' || str[0]=='i' || str[0]=='o' || str[0]=='u'){
+        return true;
+      }
+      else{
+        return false;
+      }
+    }
+    
+stringVowels(3, 'abc')     
