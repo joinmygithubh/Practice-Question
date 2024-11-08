51fd1c2f08e7d6aa30043188a35cdedaf464cd51
Sameer Ahmed <sameerahmed1601@gmail.com>
2024-11-08 15:13:41 +0530

    Traversal

--- traversalOnBorder.js
+++ traversalOnBorder.js
@@ -3,7 +3,6 @@ let arr = [ [1,2,3,4],
             [9,10,11,12],
             [13,14,15,16]]
 
-
 for(let i=0; i<arr.length; i++){
     for(let j=0; j<arr[i].length; j++){
         if(i==0 || j==0 || j==arr.length-1 || i==arr.length-1){
