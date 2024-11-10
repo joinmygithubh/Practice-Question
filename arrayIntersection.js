5c929970bef2054096ca2add88ee7181710a676d
Sameer Ahmed <sameerahmed1601@gmail.com>
2024-11-10 21:02:23 +0530

    Array Intersection

---
+++ arrayIntersection.js
@@ -0,0 +1,18 @@
+function intersectionOfArray(N, arr1, arr2){
+    //write code here
+    for(let i=0; i<N; i++){
+      for(let j=0; j<N; j++){
+        if(arr1[i]==arr2[j]){
+          console.log(arr1[i]);
+        }
+      }
+    }
+}
+
+
+let N =3;
+let arr1 = [4,5,7]
+let arr2 = [9,2,5]
+
+
+intersectionOfArray(3, arr1,arr2)
