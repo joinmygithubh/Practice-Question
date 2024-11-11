3fbd92fdd4d396aeedd30854a572aa571e9644a3
Sameer Ahmed <sameerahmed1601@gmail.com>
2024-11-11 21:16:55 +0530

    Library Management system

---
+++ LibraryManagementSystem.js
@@ -0,0 +1,83 @@
+class Book{
+    #ISBN;
+
+    constructor(title, author, ISBN, availableCopies){
+        this.title = title
+        this.author = author
+        this.#ISBN = ISBN;
+        this.availableCopies = availableCopies;
+    }
+
+    get availableCopies(){
+        return this._availableCopies;
+    }
+    set availableCopies(copies){
+        if(typeof copies === 'number' && copies >=0){
+            this._availableCopies = copies;
+        }
+        else{
+            console.error("Invalid copies")
+        }
+    }
+    #validateISBN(ISBN){
+        return typeof ISBN === "string" && ISBN.length === 13;
+    }
+
+    get ISBN(){
+        return this.#ISBN;
+    }
+    set ISBN(newIsbn){
+        if(this.#validateISBN(newIsbn)){
+            this.#ISBN= newIsbn;
+        }
+        else{
+            console.log("Invalid ISBN");
+        }
+    }
+
+    static ISBNgeneration (){
+        return Math.floor(100000000000 + Math.random()+90000000).toString();
+    }
+}
+
+class Library{
+    constructor(){
+        this.books =[];
+    }
+    addBook(book){
+        this.books.push(book);
+    }
+    removeBook(ISBN){
+        this.books = this.books.filter(book => book.ISBN !== ISBN)
+        
+    }
+    searchBooks(query){
+        return this.books.filter(book =>
+            book.title.toLowerCase().includes(query.toLowerCase()) ||
+            book.author.toLowerCase().includes(query.toLowerCase())
+        );
+    }
+
+    displayBooks(){
+        this.books.forEach(book =>{
+            console.log("Title "+ book.title + ","+ "Author: " + book.author + "," + "ISBN:" + book.ISBN + "," + "Available Copies" + book.availableCopies )
+        })
+    }
+}
+
+let book1 = new Book("Java Basics", "Sameer", Book.ISBNgeneration(), 12)
+let book2 = new Book("C++", "same", Book.ISBNgeneration(),5)
+let book3 =new Book ("C sharp", "integrated",Book.ISBNgeneration(),12)
+
+let library = new Library();
+
+library.addBook(book1);
+library.addBook(book2)
+library.addBook(book3)
+library.displayBooks();
+
+console.log(library.searchBooks("Java Basics"))
+console.log(library.searchBooks("C sharp"))
+library.removeBook(book1.ISBN);
+
+library.displayBooks();
