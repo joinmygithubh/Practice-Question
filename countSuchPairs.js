11f8730caf5c497a10de2b8fcad2678e2f550998
Sameer Ahmed <sameerahmed1601@gmail.com>
2024-11-11 13:38:57 +0530

    Count such pairs

---
+++ countSuchPairs.js
@@ -0,0 +1,19 @@
+function countSuchPairs(N,K,A){
+    //write code here
+    let count=0;
+    for(let i=0; i<N; i++){
+      
+      for(let j=i+1; j<N; j++){
+        if(A[i]+A[j]==K){
+          count++;
+        }
+      }
+    }
+    console.log(count)
+}    
+
+let N =5;
+let K =9;
+let A = [3,0,6,2,7]
+
+countSuchPairs(N,K,A)
