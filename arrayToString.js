d88c35964b4a9156cbe22b050c05af9f37e35320
Sameer Ahmed <sameerahmed1601@gmail.com>
2024-11-13 21:10:09 +0530

    Array To String

---
+++ arrayToString.js
@@ -0,0 +1,16 @@
+function arrayToString(n,arr){
+    //write code here
+    let res = "";
+    for(let i =0; i<n ; i++){
+      if(arr[i]<0){
+        res+="0"
+      }
+      else{
+        res+= arr[i].toString();
+      }
+    }
+    console.log(res)
+}
+ 
+let arr =["2", "4", "6", "8", "9"]
+arrayToString(5,arr)
