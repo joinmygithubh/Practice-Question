c7d8beaf7404d3a38b565a23266f26597a5fb03f
Sameer Ahmed <sameerahmed1601@gmail.com>
2024-11-13 21:21:59 +0530

    Library Management

--- LibraryManagementSystem.js
+++ LibraryManagementSystem.js
@@ -64,7 +64,6 @@ class Library{
         })
     }
 }
-
 let book1 = new Book("Java Basics", "Sameer", Book.ISBNgeneration(), 12)
 let book2 = new Book("C++", "James Goslin", Book.ISBNgeneration(),5)
 let book3 =new Book ("C sharp", "James Goslin",Book.ISBNgeneration(),12)
