9815945f310d1e0bc98901d98995b21b2648e916
Sameer Ahmed <sameerahmed1601@gmail.com>
2024-11-14 17:05:13 +0530

    Card and Values Updated

--- cardsAndValues.js
+++ cardsAndValues.js
@@ -1,7 +1,7 @@
 let arr = [1,2,3,4]
 let N = 4
 
-for(let i=0; i<N;i++){
+for(let i=0; i<N-1;i++){
     for(let j=i+1; j<N; j++){
         if(arr[i]*2 == arr[j] || arr[j]*2== arr[i]){
             console.log("Yes")
--- cardsAndValues2.js
+++ cardsAndValues2.js
@@ -2,7 +2,7 @@ let arr=[5,0,9,7,20]
 let N=5;
 
 function cards(arr,N){
-    for(let i=0; i<N; i++){
+    for(let i=0; i<N-1; i++){
         for(let j=i+1; j<N; j++){
             if(arr[i]*2 == arr[j] || arr[j]*2== arr[i] ){
                 console.log("Yes")
