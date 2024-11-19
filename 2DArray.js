52b81478a7dfeffc2aa54dc4cdcbda9cd1bda4cc
Sameer Ahmed <sameerahmed1601@gmail.com>
2024-11-19 15:18:37 +0530

    2 D array

---
+++ 2DArray.js
@@ -0,0 +1,27 @@
+let arr = [[3, 5, 6], [9, 2, 6], [3, 5, 2]]
+let ans = 0
+
+
+for(let i=0 ; i<=2; i++){
+  if(arr[0][i] %3==0)
+  ans +=1;
+}
+
+let column =1
+
+for(let i=1; i<=2; i++){
+  if(arr[i][column]%3==0){
+    ans += i
+  }
+  column--
+}
+
+for(let i=1; i<=2; i++){
+  if(arr[2][i]%3==0){
+    ans +=i
+  }
+}
+
+
+console.log(ans)
+
