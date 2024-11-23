488a2081b4193c180610a98be43fd2e8c85f75af
Sameer Ahmed <sameerahmed1601@gmail.com>
2024-11-23 16:51:07 +0530

    Z Traversal

---
+++ Z_Traversal.js
@@ -0,0 +1,20 @@
+function zTraversal(N, arr) {
+    // Write code here
+    let temp = "";
+
+    for (let j = 0; j < N; j++) {
+        temp += arr[0][j] + " ";
+    }
+    for (let i = 1; i < N - 1; i++) {
+        temp += arr[i][N - i - 1] + " ";
+    }
+    for (let j = 0; j < N; j++) {
+        temp += arr[N - 1][j] + " ";
+    }
+
+    console.log(temp);
+}
+
+let arr = [[1,2,3],[4,5,6],[7,8,9]]
+
+zTraversal(3, arr)
