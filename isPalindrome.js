484f117cd28b25b7c9be360c8c81dc125593c65a
Sameer Ahmed <sameerahmed1601@gmail.com>
2024-11-30 22:35:39 +0530

    Is Palindrome

---
+++ isPalindrome.js
@@ -0,0 +1,14 @@
+function isPalindrome(str) {
+    let i=0; 
+    let j= str.length-1;
+    while(i<j){
+      if(str[i]!=str[j]){
+        console.log("No")
+      }
+      i++;
+      j--;
+    }
+    console.log("Yes")
+  }
+
+  isPalindrome("Nitin")
