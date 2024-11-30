bc65d6b5b9fdd912b2a8e967d7dc5b871f0770e3
Sameer Ahmed <sameerahmed1601@gmail.com>
2024-11-30 22:47:59 +0530

    No of ping

---
+++ numberOfPing.js
@@ -0,0 +1,20 @@
+function numberOfPing(n,str){
+    //write code here
+    let countOfCharacters = { 'p' : 0, 'i': 0, 'n':0, 'g': 0};
+    
+    for(let char of str){
+      if(char in countOfCharacters){
+        countOfCharacters[char]++;
+      }
+    }
+    console.log (Math.min(countOfCharacters['p'], countOfCharacters['i'], countOfCharacters['n'], countOfCharacters['g']))
+  }
+
+let n= 7;
+let str = "pingpin"
+
+let n1 =8;
+let str1= "pingping"
+
+numberOfPing(n,str)
+numberOfPing(n1,str1)
