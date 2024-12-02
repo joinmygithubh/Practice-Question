5aa1766af002b920810dc5a5b588ede7ce4e6128
Sameer Ahmed <sameerahmed1601@gmail.com>
2024-12-02 20:28:22 +0530

    Merge Sorted Array Brute Force

---
+++ mergeSortedArrays.js
@@ -0,0 +1,17 @@
+//Brute Force Approach
+
+let arr1 = [1,3,5,7,9]
+let arr2 = [2,4,6,8,10]
+
+let res =[];
+
+for(let i=0; i<arr1.length; i++){
+    res.push(arr1[i])
+}
+for(let i=0; i<arr2.length;i++){
+    res.push(arr2[i])
+}
+console.log(res.sort((a,b)=>a-b)); // T.C = O(nlogn)
+
+//Time Complexity = O(nlogn);
+//Space Complexiy = O(n+m)
