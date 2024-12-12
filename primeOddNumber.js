3693e6a827e47d2be6f996b0f41d0cc405f9cffd
Sameer Ahmed <sameerahmed1601@gmail.com>
2024-12-12 23:29:26 +0530

    Prime Odd Number

--- primeOddNumber.js
+++ primeOddNumber.js
@@ -1,15 +1,22 @@
 function primeOddNumber(num){
-    for(let i=0; i<=num; i++){
-        console.log(i)
+    
+    for(let i=3; i<=num; i+=2){
+        let isPrime =true;
+        for(let j=2; j*j<=i; j++){
+            if(i%j==0){
+                isPrime = false;
+                break;
+            }
+        }
+        if(isPrime){
+            console.log(i)
+        }
     }
+    
 }
 
 
 
 
-
-
-
-
-
-let num =21;
+let num =15;
+primeOddNumber(num)
