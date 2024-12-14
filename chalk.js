cfb14d0bd15e0ee8f82a1f2a98e7d83a7422787a
Sameer Ahmed <sameerahmed1601@gmail.com>
2024-12-14 23:52:58 +0530

    Chalk

--- chalk.js
+++ chalk.js
@@ -6,7 +6,7 @@
   
     // Display the warning message "Caution: Low Disk Space" in yellow
     console.log(chalk.default.yellow('Caution: Low Disk Space'));
-  
+    
     // Show the success message "Installation Complete!" in green
     console.log(chalk.default.green('Installation Complete!'));
   })();
