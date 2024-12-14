56e405cd5ed787556d949066504c937a6e37f0a9
Sameer Ahmed <sameerahmed1601@gmail.com>
2024-12-14 23:35:58 +0530

    Smalle Neighbour Element

---
+++ smallerNeighbourELement.js
@@ -0,0 +1,25 @@
+function runProgram(input) {
+    let lines = input.trim().split("\n");
+    let N = parseInt(lines[0]);
+    let arr = lines[1].trim().split(" ").map(Number);
+
+    let stack = [];
+    let result = [];
+
+    for (let i = 0; i < N; i++) {
+        while (stack.length > 0 && arr[stack[stack.length - 1]] >= arr[i]) {
+            stack.pop();
+        }
+
+        if (stack.length === 0) {
+            result.push(-1);
+        } else {
+            result.push(arr[stack[stack.length - 1]]);
+        }
+
+        stack.push(i);
+    }
+
+    console.log(result.join(" "));
+}
+
