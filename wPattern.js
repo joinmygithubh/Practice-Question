c249394a0f53aca260b32f5c881650583c1d1789
Sameer Ahmed <sameerahmed1601@gmail.com>
2024-12-23 23:51:07 +0530

    W pattern

---
+++ wPattern.js
@@ -0,0 +1,37 @@
+function patternW(N) {
+    // Write code here
+    for(i=0;i<N;i++){
+      let output = "";
+      for(j=0;j<N*2;j++){
+        if(i==j){
+          output+="\\"
+        }
+        else{
+          if(j+1==N*2-i){
+            output+= '/'
+          }
+          else{
+            output+=" "
+          }
+        }
+      }
+      for(j=0;j<N*2;j++){
+        if(i==j){
+          output+='\\'
+        }
+        else{
+          if(j+1==N*2-i){
+            output+='/';
+            break;
+          }
+          else{
+            output+=" "
+          }
+        }
+      }
+      console.log(output)
+    }
+    
+}
+
+patternW(4)
