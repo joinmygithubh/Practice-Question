97b7e07be2de408a7a4120c9c2486b761c2c15f3
Sameer Ahmed <sameerahmed1601@gmail.com>
2024-12-27 23:57:39 +0530

    Smaller Neighbour

--- smallerNeighbourELement.js
+++ smallerNeighbourELement.js
@@ -6,7 +6,6 @@ function runProgram(input) {
     let stack = [];
     let result = [];
 
-    
     for (let i = 0; i < N; i++) {
         while (stack.length > 0 && arr[stack[stack.length - 1]] >= arr[i]) {
             stack.pop();
