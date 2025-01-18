10e020bc638d9a63fdc531566e4d7121922e2450
Sameer Ahmed <sameerahmed1601@gmail.com>
2025-01-18 22:48:15 +0530

    Even Subarrays Sum

---
+++ countEvenSubArraySum.js
@@ -0,0 +1,18 @@
+function countEvenSumSubarrays(arr, n) {
+    let evenCount = 1; 
+    let oddCount = 0;
+    let currentSum = 0;
+    let result = 0;
+
+    for (let i = 0; i < n; i++) {
+        currentSum += arr[i];
+        if (currentSum % 2 === 0) {
+            result += evenCount;
+            evenCount++;
+        } else {
+            result += oddCount;
+            oddCount++;
+        }
+    }
+    console.log(result)
+}
