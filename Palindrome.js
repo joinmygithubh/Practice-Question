57eaa1f5fb289b82ba337f1419e057f73b466d6d
Sameer Ahmed <sameerahmed1601@gmail.com>
2025-01-19 23:12:21 +0530

    Palindrome

--- Palindrome.js
+++ Palindrome.js
@@ -1,2 +1,18 @@
-let num = [121];
+var isPalindrome = function(x) {
+    if(x<0){
+        return false
+    }
+    let str = x.toString();
+    let res = ''
 
+    for(let i=str.length-1; i>=0; i--){
+        res += str[i];
+    }
+
+    if(x==res){
+        return true
+    }
+    else{
+        return false
+    }
+};
