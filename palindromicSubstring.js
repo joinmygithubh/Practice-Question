fd49cee9ca1761a8b23d65d0f96f562f1dc827f6
Sameer Ahmed <sameerahmed1601@gmail.com>
2025-01-20 23:42:06 +0530

    Palindromic Substring

--- palindromicSubstring.js
+++ palindromicSubstring.js
@@ -31,8 +31,9 @@ var isPalindrome = function (s) {
 // Input: s = "abc"
 // Output: 3
 // Explanation: Three palindromic strings: "a", "b", "c".
-// Example 2:
 
+
+// Example 2:
 // Input: s = "aaa"
 // Output: 6
 // Explanation: Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa".
