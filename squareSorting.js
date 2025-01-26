031f959a5a506ae1503db86f113befe289b1fbcc
Sameer Ahmed <sameerahmed1601@gmail.com>
2025-01-26 23:13:00 +0530

    Square Sorting

---
+++ squareSorting.js
@@ -0,0 +1,24 @@
+function squareSorting(n, arr){
+    for(let i=0; i<n ; i++){
+        let temp=0;
+        for(let j=0; j<n ;j++){
+            if(arr[j]*arr[j] > arr[j+1]*arr[j+1]){
+                temp = arr[j]
+                arr[j] = arr[j+1]
+                arr[j+1] = temp
+                temp++
+            }
+        }
+        if(temp==0){
+            break;
+        }
+    }
+    console.log(arr)
+}
+
+
+let n = 5
+let arr = [-2, 3 ,1, -4, 6]
+
+squareSorting(n, arr)
+
