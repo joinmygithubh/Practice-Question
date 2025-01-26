7656cd0bb274ba4723207e996983b2ad3f54fd99
Sameer Ahmed <sameerahmed1601@gmail.com>
2025-01-26 23:08:27 +0530

    Selection Sort

---
+++ selectionSort.js
@@ -0,0 +1,20 @@
+function selectionSort(n, arr){
+    for(let i=0; i<n; i++){
+        minIndex = i
+
+        for(let j=i+1; j<n ;j++){
+            if(arr[minIndex] > arr[j]){
+                minIndex = j
+            }
+        }
+        temp = arr[i]
+        arr[i] = arr[minIndex]
+        arr[minIndex] = temp
+    }
+    console.log(arr.join(" "))
+}
+
+let n = 5
+let arr = [3, 5, 0, 9, 8]
+
+selectionSort(n, arr)
