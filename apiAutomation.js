b4ef90dccd0529d491f5d58a02a1eb2810c70cc5
Sameer Ahmed <sameerahmed1601@gmail.com>
2025-01-27 23:38:00 +0530

    MoviesFinder

---
+++ apiAutomation.js
@@ -0,0 +1,31 @@
+let axios = require('axios')
+
+let BASE_url = 'https://jsonplaceholder.typicode.com/posts'
+
+async function apiTesting(){
+    try {
+        let res = await axios.get(BASE_url)
+        console.log("Title of the first post:" , res.data[0].title);
+
+        let newPost = {
+            title: 'New post',
+            body : "Testing automated api testing" ,
+            userId : 123
+
+        }
+        let newPost2 = {
+            title: 'New post',
+            body : "Testing automated api testing" ,
+            userId : 124
+
+        }
+
+        let postResponse  = await axios.post(BASE_url, newPost, newPost2);
+        console.log("Response from the post", postResponse.data)
+
+    } catch (error) {
+        console.error('Error occured while testing the API', error.message)
+    }
+}
+
+apiTesting()
