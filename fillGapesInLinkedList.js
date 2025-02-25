0d084239fc1886d2b1cc186014a8b626d26ad039
Sameer Ahmed <sameerahmed1601@gmail.com>
2025-02-25 23:32:24 +0530

    Fill Gapes

---
+++ fillGapesInLinkedList.js
@@ -0,0 +1,26 @@
+const LinkedListNode = class {
+    constructor(nodeData) {
+        this.data = nodeData;
+        this.next = null;
+    }
+};
+// Complete the function below
+var fillGaps = function (head) {
+    if (!head) return null; 
+
+    let current = head;
+
+    while (current && current.next) {
+        if (current.next.data - current.data > 1) {
+        
+            let newNode = new LinkedListNode(current.data + 1);
+            newNode.next = current.next;
+            current.next = newNode;
+        } else {
+  
+            current = current.next;
+        }
+    }
+
+    return head;
+};
