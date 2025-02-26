6e2b0ed3a5db50957a29bfad893b5e7a61fb632d
Sameer Ahmed <sameerahmed1601@gmail.com>
2025-02-26 23:28:44 +0530

    Power sum Recursion

---
+++ powerSumRecursion.js
@@ -0,0 +1,11 @@
+function runProgram(input) {
+    let [X, N] = input.trim().split(" ").map(Number);
+    console.log(countWays(X, N, 1));
+}
+
+function countWays(X, N, num) {
+    let power = Math.pow(num, N);
+    if (X === 0) return 1;
+    if (X < 0 || power > X) return 0;
+    return countWays(X - power, N, num + 1) + countWays(X, N, num + 1);
+}
