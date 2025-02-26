49228c9fa680172f22097bbd09ed3afcf10605d9
Sameer Ahmed <sameerahmed1601@gmail.com>
2025-02-26 23:18:40 +0530

    Power Function Recursive

--- powerFunctionRecursive.js
+++ powerFunctionRecursive.js
@@ -2,7 +2,6 @@ function runProgram(input) {
     let [a, b] = input.split(" ").map(Number);
     console.log(power(a, b));
 }
-
 function power(a, b) {
     if (b === 0) return 1; 
     let halfPower = power(a, Math.floor(b / 2));
