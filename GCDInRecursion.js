40ed10642a8f56ddd82c743223d345224f7dcb7a
Sameer Ahmed <sameerahmed1601@gmail.com>
2025-02-28 21:37:17 +0530

    Greatest Common Divisor

---
+++ GCDInRecursion.js
@@ -0,0 +1,14 @@
+function gcd(a, b) {
+    if (b === 0) return a; 
+    return gcd(b, a % b);  
+}
+
+function runProgram(input) {
+    let lines = input.trim().split("\n");
+    let t = parseInt(lines[0]);  
+    
+    for (let i = 1; i <= t; i++) {
+        let [a, b] = lines[i].split(" ").map(Number);
+        console.log(gcd(a, b)); 
+    }
+}
