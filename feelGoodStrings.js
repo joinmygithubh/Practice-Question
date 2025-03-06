8bde86e50a24b7d228c99eff918bce73196933bf
Sameer Ahmed <sameerahmed1601@gmail.com>
2025-03-06 22:25:07 +0530

    Feel good Strings

---
+++ feelGoodStrings.js
@@ -0,0 +1,18 @@
+function feelGoodStrings(N, string) {
+    let vowels = new Set(['a', 'e', 'i', 'o', 'u']);
+    let vowelCount = 0;
+
+    // Count the number of vowels in the string
+    for (let char of string) {
+        if (vowels.has(char)) {
+            vowelCount++;
+        }
+    }
+
+    // Check if vowel count is greater than or equal to N/2
+    if (vowelCount >= N / 2) {
+        console.log("Feel good!");
+    } else {
+        console.log("Feel bad!");
+    }
+}
