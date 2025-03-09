fe52a853a66a869e862462af8987e0bfe3512729
Sameer Ahmed <sameerahmed1601@gmail.com>
2025-03-09 15:15:05 +0530

    Discover Number Binary Search

---
+++ discoverNumber-Binary Search.js
@@ -0,0 +1,12 @@
+function runProgram(input) {
+    let lines = input.trim().split("\n");
+    let [N, Q] = lines[0].split(" ").map(Number);
+    let A = new Set(lines[1].split(" ").map(Number));
+    let result = [];
+
+    for (let i = 2; i < 2 + Q; i++) {
+        result.push(A.has(Number(lines[i])) ? "YES" : "NO");
+    }
+
+    console.log(result.join("\n"));
+}
