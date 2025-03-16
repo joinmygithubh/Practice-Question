7c5b88503782dac47d2cb1fd63e3e29d6f0d94cb
Sameer Ahmed <sameerahmed1601@gmail.com>
2025-03-16 23:23:06 +0530

    Missing number in Array

---
+++ missingNumberInArray.js
@@ -0,0 +1,9 @@
+function findMissingNumber(arr) {
+    const n = arr.length + 1;
+    const sum = (n * (n + 1)) / 2;
+    const arrSum = arr.reduce((acc, num) => acc + num, 0);
+    return sum - arrSum;
+  }
+  
+  const numbers = [1, 2, 3, 5];
+  console.log(findMissingNumber(numbers));  // Output: 4
