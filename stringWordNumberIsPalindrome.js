292144576667dd35fc9c5efa629fc8321f30a84d
Sameer Ahmed <sameerahmed1601@gmail.com>
2025-03-19 23:46:06 +0530

    Check String, Word or number is Palindrome

---
+++ stringWordNumberIsPalindrome.js
@@ -0,0 +1,12 @@
+const isPlaindrome = (inputChar) => {
+    let str = inputChar.toString();
+    let resultWord = '';
+    for(let i=str.length-1; i>=0; i--)
+    {
+        resultWord += str[i];
+    }
+    return (resultWord == str) ? true : false;
+}
+console.log(isPlaindrome('racecar'))
+console.log(isPlaindrome('abc'))
+console.log(isPlaindrome(121))
