d2dc5e0a271841a36c1723b68963c6f3ee3c20bc
Sameer Ahmed <sameerahmed1601@gmail.com>
2025-03-22 23:32:14 +0530

    A

--- stringWordNumberIsPalindrome.js
+++ stringWordNumberIsPalindrome.js
@@ -8,6 +8,7 @@ const isPlaindrome = (inputChar) => {
     return (resultWord == str) ? true : false;
 }
 
+
 console.log(isPlaindrome('racecar'))
 console.log(isPlaindrome('abc'))
 console.log(isPlaindrome(121))
