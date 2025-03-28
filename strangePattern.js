f1752a8cd54ae839243e855810a23a9e029bcd6d
Sameer Ahmed <sameerahmed1601@gmail.com>
2025-03-28 23:42:40 +0530

    Strange Pattern

--- strangePattern.js
+++ strangePattern.js
@@ -9,13 +9,13 @@ function strangePattern(N,M){
     }
     pattern1 += "+";
     pattern2 += "+";
-
     
     for (let i = 0; i < N; i++) {
       console.log(pattern1);
       console.log(pattern2);
     }
   }
+  
 strangePattern(3,4)
     
   
