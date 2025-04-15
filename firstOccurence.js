6d6fc29006dcce653930c2bbad0cf665ce0dcbf1
Sameer Ahmed <sameerahmed1601@gmail.com>
2025-04-15 23:46:37 +0530

    Fill Gaps

---
+++ firstOccurence.js
@@ -0,0 +1,13 @@
+function firstOccurence(arr, n , key){
+    let low = 0
+    let high = n
+    let res =1
+
+    while(low<=high){
+        let mid = Math.floor(low + (low- high)/2)
+
+        if(key = arr[mid]){
+            
+        }
+    }
+}
