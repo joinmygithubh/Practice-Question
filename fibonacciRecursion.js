3368d6036330a187f9d6669d358a711a4259e9ab
Sameer Ahmed <sameerahmed1601@gmail.com>
2025-05-14 22:13:09 +0530

    Fibonacci

--- fibonacciRecursion.js
+++ fibonacciRecursion.js
@@ -3,8 +3,6 @@ function fibonacci(n) {
     if (n === 1) return 1;  
     return fibonacci(n - 1) + fibonacci(n - 2);  
 }
-
-
 function runProgram(input) {
     let n = parseInt(input.trim());
     console.log(fibonacci(n));
