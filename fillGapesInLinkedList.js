e04a22c78f15abaa59ebfbcccae66dcdb0b8d265
Sameer Ahmed <sameerahmed1601@gmail.com>
2025-05-24 23:56:20 +0530

    A

--- fillGapesInLinkedList.js
+++ fillGapesInLinkedList.js
@@ -14,6 +14,7 @@ var fillGaps = function (head) {
             newNode.next = current.next;
             current.next = newNode;
         } else {
+
             
             current = current.next;
         }   
