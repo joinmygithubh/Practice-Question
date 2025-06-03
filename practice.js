a056e24b83d73d2e5093a9ab31f23e01409f827f
Sameer Ahmed <sameerahmed1601@gmail.com>
2025-06-03 23:52:09 +0530

    Practice

---
+++ practice.js
@@ -0,0 +1,19 @@
+// let students = [
+//   { name: "Atharva", grade: 85, age: 25 },
+//   { name: "Pritam", grade: 92, age: 23 },
+//   { name: "Sahil", grade: 78, age: 26 },
+//   { name: "Swapnil", grade: 80, age: 22 },
+//   { name: "Kuldeep", grade: 75, age: 24 }
+// ];
+
+// let result = students
+//   .filter(student => student.grade > 75)
+//   .sort((a, b) => b.grade - a.grade)
+//   .map(student => student.name);
+
+// console.log(result);
+
+let num = [1,2,3,4,5]
+num.forEach(num => {
+  console.log(num * 2);
+});
