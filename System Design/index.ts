7c9252ecfdf2cf384267aeede91ee3f017a10771
Sameer Ahmed <sameerahmed1601@gmail.com>
2025-06-30 22:18:43 +0530

    System

---
+++ System Design/index.ts
@@ -0,0 +1,25 @@
+"use Stric"
+
+console.log("this is my first ts file")
+
+
+let a: number = 20
+let b: string = "System Design"
+let c: boolean = true;
+
+let array:number[] = [1,2,3,4];
+
+console.log(a)
+console.log(b)
+console.log(c)
+console.log()
+
+//Types =  any , void, unknown, null, undefined
+
+function users() : void {
+    console.log("we are system design students")
+
+    return "we are system desing students"
+} 
+
+users()
