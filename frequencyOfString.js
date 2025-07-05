3f7c86b0f9a48dcad79079f962b3bfc6512c56be
Sameer Ahmed <sameerahmed1601@gmail.com>
2025-07-05 23:35:45 +0530

    Frequency of a String

---
+++ frequencyOfString.js
@@ -0,0 +1,20 @@
+//Frequency of string means how many time each character appears in String
+
+let name = 'Sammeer'
+
+let ob = {}
+
+for(let i=0; i<name.length; i++){
+    let char = name[i];
+
+    if(ob[char]){
+    ob[char]++
+}
+else{
+    ob[char] =1
+}
+
+}
+
+
+console.log(ob)
