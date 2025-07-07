53d1996ab83d9abc4c956e86ebf974898fb1a19f
Sameer Ahmed <sameerahmed1601@gmail.com>
2025-07-07 23:59:22 +0530

    A

--- GCDInRecursion.js
+++ GCDInRecursion.js
@@ -6,6 +6,7 @@ function runProgram(input) {
     let lines = input.trim().split("\n");
     let t = parseInt(lines[0]);  
     
+    
     for (let i = 1; i <= t; i++) {
         let [a, b] = lines[i].split(" ").map(Number);
         console.log(gcd(a, b)); 
