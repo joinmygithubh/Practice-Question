3c0056a8f3c01b047406fdfbafe986ae7398a481
Sameer Ahmed <sameerahmed1601@gmail.com>
2025-07-09 23:50:00 +0530

    Lexographical frequency String

---
+++ lexographicalFrequencyString.js
@@ -0,0 +1,22 @@
+function lexographicalfrequencyString(s){
+    let freq = {}
+
+    for(let i=0 ;i<s.length; i++){
+        let char = s[i]
+
+        if(freq[char]){
+            freq[char]++
+        }
+        else{
+            freq[char]=1
+        }
+    }
+
+    let sortedkey = Object.keys(freq).sort()
+
+    for(let key of sortedkey){
+        console.log(key + " " + freq[key])
+    }
+}
+
+let s = 'masai'
