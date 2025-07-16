7c57d7c39c3bfe295b4b539d9a278f3f19d22fea
Sameer Ahmed <sameerahmed1601@gmail.com>
2025-07-16 22:38:12 +0530

    Linked list has a cycle

--- linkedListHasACycle.js
+++ linkedListHasACycle.js
@@ -11,7 +11,8 @@ function hasCycle(head) {
     return false;
   }
   
-  // Example linked list with a cycle
   
+  // Example linked list with a cycle
+
   const head = { value: 1, next: { value: 2, next: { value: 3, next: head } } };
   console.log(hasCycle(head));  // Output: true
