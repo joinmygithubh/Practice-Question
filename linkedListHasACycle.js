621bdcf781ad12a66237b87aa9c622b7550b9297
Sameer Ahmed <sameerahmed1601@gmail.com>
2025-07-17 22:06:35 +0530

    Linked

--- linkedListHasACycle.js
+++ linkedListHasACycle.js
@@ -10,8 +10,9 @@ function hasCycle(head) {
     }
     return false;
   }
-  
 
+
+  
   // Example linked list with a cycle
 
   const head = { value: 1, next: { value: 2, next: { value: 3, next: head } } };
