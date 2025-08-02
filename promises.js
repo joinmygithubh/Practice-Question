c381f5a211003be999cecef4c93354a86e1c201e
Sameer Ahmed <sameerahmed1601@gmail.com>
2025-08-02 19:47:21 +0530

    Promises

---
+++ promises.js
@@ -0,0 +1,19 @@
+function fetchData(){
+    return new Promise((resolve, reject)=>{
+        let fetchData = false
+        if(fetchData){
+             resolve('Data fetch Successfully')
+        }
+        else{
+            reject('something went wrong')
+        }
+    })
+}
+
+let promise = fetchData()
+.then((result)=>{
+    console.log(result)
+})
+.catch((error)=>{
+    console.log(error)
+})
