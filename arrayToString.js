1232e97bf562acd4f3050d810cdec6f9cf48f2f8
Sameer Ahmed <sameerahmed1601@gmail.com>
2025-08-10 23:09:08 +0530

    A

--- arrayToString.js
+++ arrayToString.js
@@ -5,14 +5,13 @@ function arrayToString(n,arr){
       if(arr[i]<0){
         res+="0"
       }
-      
+
       else{
         res+= arr[i].toString();
       }
     }
     console.log(res)
 }
-
  
 let arr =["2", "4", "6", "8", "9"]
 arrayToString(5,arr)
