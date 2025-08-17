8356b777bb5373aa4e471a87be91a37e23ee5298
Sameer Ahmed <sameerahmed1601@gmail.com>
2025-08-17 23:24:10 +0530

    Area

---
+++ dbmongo.js
@@ -0,0 +1,12 @@
+const mongodb = require('mongodb')
+const mongoose = require('mongoose')
+const express = require('express')
+
+const app = express();
+
+const db = mongoose.connect().then(()=>{})
+
+
+app.listen(()=>{
+    
+})
