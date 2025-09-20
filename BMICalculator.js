82ca1a70208c0bf980969802784a51383138cdb7
Sameer Ahmed <sameerahmed1601@gmail.com>
2025-09-20 21:35:59 +0530

    BMI

--- BMICalculator.js
+++ BMICalculator.js
@@ -8,7 +8,6 @@ function calculateBMI(weight, height) {
     let bmi = weight / (height * height);
     return `Your BMI is: ${bmi.toFixed(2)}`;
 }
-
 // Test cases
 let weight1 = 70, height1 = 1.75;
 console.log(calculateBMI(weight1, height1)); // o/p: "Your BMI is: 22.86"
