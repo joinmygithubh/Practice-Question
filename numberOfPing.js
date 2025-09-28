9a540eeff5b2cbb52c74ae5f408826ef2422ee69
Sameer Ahmed <sameerahmed1601@gmail.com>
2025-09-28 23:51:56 +0530

    Number

--- numberOfPing.js
+++ numberOfPing.js
@@ -9,6 +9,8 @@ function numberOfPing(n,str){
     }
     console.log (Math.min(countOfCharacters['p'], countOfCharacters['i'], countOfCharacters['n'], countOfCharacters['g']))
   }
+
+  
 let n= 7;
 let str = "pingpin"
 let n1 =8;
